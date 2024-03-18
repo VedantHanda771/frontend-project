32b998cac1a445c8122037b212631df348872133
VedantHanda771 <vedanthanda123@gmail.com>
2024-03-18 10:55:03 +0530

    1st_evual

---
+++ app.js
@@ -0,0 +1,10 @@
+function showSlidebar(){
+    const slidebar=document.querySelector('#slidebar')
+    slidebar.style.display = 'flex'
+}
+
+function hideSlidebar(){
+    const slidebar=document.querySelector('#slidebar');
+    slidebar.style.display = 'none'
+
+}
